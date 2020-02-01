545eeda6a947a7a9a220cfc6671804cd522a5f49
Carlos <carlos.santos112@outlook.com>
2020-01-31 18:10:20 -0800

    json tokens
    working on adding the rest of the json token listed in the json.h header file.

--- HW_1_FLEX/lex.yy.c
+++ HW_1_FLEX/lex.yy.c
@@ -373,8 +373,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
+#define YY_NUM_RULES 11
+#define YY_END_OF_BUFFER 12
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -382,9 +382,11 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[10] =
+static const flex_int16_t yy_accept[26] =
     {   0,
-        0,    0,    5,    3,    4,    1,    1,    2,    0
+        0,    0,   12,   10,   11,    9,    3,    5,    6,   10,
+        9,    7,    8,    3,    4,    0,    8,    0,    0,    0,
+        0,    0,    1,    2,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -392,17 +394,17 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    3,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    3,    1,    1,    1,    1,    1,    1,
+        1,    4,    3,    5,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    6,    1,
+        1,    1,    1,    1,    7,    1,    1,    1,    8,    9,
+        1,    1,    1,    1,    1,   10,    1,    1,    1,    1,
+        1,   11,   12,   13,   14,    1,    1,    1,    1,    1,
+       15,    1,   16,    1,    1,    1,    1,    1,    1,    1,
 
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    4,    1,    5,    1,    1,    1,    1,    1,
+        1,    1,   17,    1,   18,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -419,37 +421,50 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static const YY_CHAR yy_meta[6] =
+static const YY_CHAR yy_meta[19] =
     {   0,
-        1,    1,    1,    1,    1
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1
     } ;
 
-static const flex_int16_t yy_base[10] =
+static const flex_int16_t yy_base[26] =
     {   0,
-        0,    0,    6,    7,    7,    7,    7,    7,    7
+        0,    0,   28,   33,   33,   33,   15,   33,   33,   20,
+       15,   33,   33,   33,   33,   16,   33,   15,   10,   11,
+       14,   13,   33,   33,   33
     } ;
 
-static const flex_int16_t yy_def[10] =
+static const flex_int16_t yy_def[26] =
     {   0,
-        9,    1,    9,    9,    9,    9,    9,    9,    0
+       25,    1,   25,   25,   25,   25,   25,   25,   25,   25,
+       25,   25,   25,   25,   25,   25,   25,   25,   25,   25,
+       25,   25,   25,   25,    0
     } ;
 
-static const flex_int16_t yy_nxt[13] =
+static const flex_int16_t yy_nxt[52] =
     {   0,
-        4,    5,    6,    7,    8,    9,    3,    9,    9,    9,
-        9,    9
+        4,    5,    6,    7,    8,    9,    4,    6,   10,    6,
+        4,    4,   11,    4,   12,   13,   14,   15,   16,   16,
+       24,   23,   22,   21,   20,   19,   18,   25,   25,   25,
+       17,   17,    3,   25,   25,   25,   25,   25,   25,   25,
+       25,   25,   25,   25,   25,   25,   25,   25,   25,   25,
+       25
     } ;
 
-static const flex_int16_t yy_chk[13] =
+static const flex_int16_t yy_chk[52] =
     {   0,
-        1,    1,    1,    1,    1,    3,    9,    9,    9,    9,
-        9,    9
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    7,   16,
+       22,   21,   20,   19,   18,   11,   10,    3,    0,    0,
+        7,   16,   25,   25,   25,   25,   25,   25,   25,   25,
+       25,   25,   25,   25,   25,   25,   25,   25,   25,   25,
+       25
     } ;
 
 /* Table of booleans, true if rule could match eol. */
-static const flex_int32_t yy_rule_can_match_eol[5] =
+static const flex_int32_t yy_rule_can_match_eol[12] =
     {   0,
-0, 0, 0, 0,     };
+0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,     };
 
 static yy_state_type yy_last_accepting_state;
 static char *yy_last_accepting_cpos;
@@ -473,8 +488,8 @@ char *yytext;
 */
 #line 8 "json.l"
 #include "json.h"
-#line 477 "lex.yy.c"
-#line 478 "lex.yy.c"
+#line 492 "lex.yy.c"
+#line 493 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -693,7 +708,7 @@ YY_DECL
 	{
 #line 18 "json.l"
 
-#line 697 "lex.yy.c"
+#line 712 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -720,13 +735,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 10 )
+				if ( yy_current_state >= 26 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 7 );
+		while ( yy_base[yy_current_state] != 33 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -763,24 +778,59 @@ do_action:	/* This label is used only to access EOF actions. */
 case 1:
 YY_RULE_SETUP
 #line 19 "json.l"
-{return LCURLY;}
+{return TRUE;}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
 #line 20 "json.l"
-{return RCURLY;}
+{return FALSE;}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 22 "json.l"
-{ printf("lexical error on line %d\n",yylineno); }
+#line 21 "json.l"
+{return LCURLY;}
 	YY_BREAK
 case 4:
 YY_RULE_SETUP
+#line 22 "json.l"
+{return RCURLY;}
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 23 "json.l"
+{return COMMA;}
+	YY_BREAK
+case 6:
+YY_RULE_SETUP
 #line 24 "json.l"
+{return COLON;}
+	YY_BREAK
+case 7:
+YY_RULE_SETUP
+#line 25 "json.l"
+{return LBRACKET;}
+	YY_BREAK
+case 8:
+YY_RULE_SETUP
+#line 26 "json.l"
+{return RBRACKET;}
+	YY_BREAK
+case 9:
+YY_RULE_SETUP
+#line 27 "json.l"
+{return STRINGLIT; return yytext;}
+	YY_BREAK
+case 10:
+YY_RULE_SETUP
+#line 28 "json.l"
+{ printf("lexical error: line %d\n",yylineno); }
+	YY_BREAK
+case 11:
+YY_RULE_SETUP
+#line 30 "json.l"
 ECHO;
 	YY_BREAK
-#line 784 "lex.yy.c"
+#line 834 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 	yyterminate();
 
@@ -1077,7 +1127,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 10 )
+			if ( yy_current_state >= 26 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1105,11 +1155,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 10 )
+		if ( yy_current_state >= 26 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 9);
+	yy_is_jam = (yy_current_state == 25);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1797,5 +1847,5 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 24 "json.l"
+#line 30 "json.l"
 
